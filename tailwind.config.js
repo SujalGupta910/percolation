3e4e22bcebd310059e9f4efd70ed076fd3ca98b1
sujal-gupta-secureu <sujal.gupta@secureu.in>
2025-04-30 22:26:24 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4A41C5",
+      },
+    },
+  },
+  plugins: [],
+};
